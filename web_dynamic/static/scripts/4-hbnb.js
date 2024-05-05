5766a5bb0666ad5029c4f5ed1b2c1292d5333baa
Bikaze <clmntmugisha@gmail.com>
2024-05-05 23:43:58 +0200

    Attempted task 7

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,65 @@
+$('document').ready(function () {
+  let amenities = {};
+  $('input[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenities[$(this).attr('data-id')];
+    }
+    let firstTwoAmenities = Object.values(amenities).slice(0, 2);
+    let displayedAmenities = Object.values(firstTwoAmenities).join(', ');
+    if (Object.values(amenities).length > 2) {
+        displayedAmenities += ", ...";
+    }
+    $('.amenities H4').text(displayedAmenities);
+  });
+    
+  $.ajax({
+    url: api + ':5001/api/v1/places_search/',
+    type: 'POST',
+    data: '{}',
+    contentType: 'application/json',
+    dataType: 'json',
+    success: function (data) {
+      $('SECTION.places').append(data.map(place => {
+        return `<ARTICLE>
+                  <DIV class="title">
+                    <H2>${place.name}</H2>
+                    <DIV class="price_by_night">
+                      ${place.price_by_night}
+                    </DIV>
+                  </DIV>
+                  <DIV class="information">
+                    <DIV class="max_guest">
+                      <I class="fa fa-users fa-3x" aria-hidden="true"></I>
+                      </BR>
+                      ${place.max_guest} Guests
+                    </DIV>
+                    <DIV class="number_rooms">
+                      <I class="fa fa-bed fa-3x" aria-hidden="true"></I>
+                      </BR>
+                      ${place.number_rooms} Bedrooms
+                    </DIV>
+                    <DIV class="number_bathrooms">
+                      <I class="fa fa-bath fa-3x" aria-hidden="true"></I>
+                      </BR>
+                      ${place.number_bathrooms} Bathrooms
+                    </DIV>
+                  </DIV>
+                  <DIV class="description">
+                    ${place.description}
+                  </DIV>
+                </ARTICLE>`;
+      }));
+    }
+  });  
+
+  const url = 'http://0.0.0.0:5001/api/v1/status/';
+  $.get(url, function (answer) {
+    if (answer.status == 'OK') {
+        $('div#api_status').addClass('available');
+    } else {
+        $('div#api_status').removeClass('available');
+    }
+  });
+});
